87394f9f66f50ec0e0f4e12a804f1b90fe685dc5
yvzc <yakovzts@gmail.com>
2022-11-21 21:07:36 +0300

    lesson3 task1 loop maxDivisor refactoring

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -110,12 +110,7 @@ fun minDivisor(n: Int): Int {
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int {
-    for (i in n - 1 downTo 2) {
-        if (n % i == 0) return i
-    }
-    return 1
-}
+fun maxDivisor(n: Int): Int = n / minDivisor(n)
 
 /**
  * Простая (2 балла)
