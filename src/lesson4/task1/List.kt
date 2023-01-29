81a35f539d94ceafb0aed2649caa0a92a19e4341
yvzc <yakovzts@gmail.com>
2023-01-29 23:51:01 +0300

    4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -204,7 +204,15 @@ fun factorizeToString(n: Int): String = TODO()
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    val result = mutableListOf<Int>()
+    var number = n
+    while (number > 0) {
+        result.add(number % base)
+        number /= base
+    }
+    return result.reversed()
+}
 
 /**
  * Сложная (4 балла)
